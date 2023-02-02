1b587ef05564ee8243bd5dcd41ed5c5d3ed865fc
Tiago Almeida <tiago.almeida@transfergalaxy.com>
2023-02-02 17:45:48 +0100

    Initital commit

---
+++ code.ts
@@ -0,0 +1,40 @@
+// This plugin will check all layers named with the right convention,
+// check against the corresponding keys on the Lokalise API,
+// and then update the text in Figma with the correct translations.
+
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+// Enable Figma color tokens
+figma.showUI(__html__, { themeColors: true, /* other options */ });
+
+// Resize the UI modal
+figma.ui.resize(360, 560)
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  // if (msg.type === 'create-rectangles') {
+  //   const nodes: SceneNode[] = [];
+  //   for (let i = 0; i < msg.count; i++) {
+  //     const rect = figma.createRectangle();
+  //     rect.x = i * 150;
+  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+  //     figma.currentPage.appendChild(rect);
+  //     nodes.push(rect);
+  //   }
+  //   figma.currentPage.selection = nodes;
+  //   figma.viewport.scrollAndZoomIntoView(nodes);
+  // }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
