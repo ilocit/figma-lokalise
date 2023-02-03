3cb65408845ede7d8dac09087099cd232286477e
Tiago Almeida <tiago.almeida@transfergalaxy.com>
2023-02-03 13:54:04 +0100

    Some more progress regarding fetching the translatable layers

--- code.ts
+++ code.ts
@@ -2,9 +2,27 @@
 // check against the corresponding keys on the Lokalise API,
 // and then update the text in Figma with the correct translations.
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
+/////////////////////////
+// TYPES
+/////////////////////////
+
+const types = [
+  "PAGE",
+  "TEXT"
+];
+
+/////////////////////////
+// VARIABLE DECLARATION
+/////////////////////////
+
+const projectID = "";
+const APItoken = "";
+
+let translatableLayersArray = {};
+
+/////////////////////////
+// FIGMA PLUGIN INIT
+/////////////////////////
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
@@ -21,20 +39,22 @@ figma.ui.resize(360, 560)
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  // if (msg.type === 'create-rectangles') {
-  //   const nodes: SceneNode[] = [];
-  //   for (let i = 0; i < msg.count; i++) {
-  //     const rect = figma.createRectangle();
-  //     rect.x = i * 150;
-  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  //     figma.currentPage.appendChild(rect);
-  //     nodes.push(rect);
-  //   }
-  //   figma.currentPage.selection = nodes;
-  //   figma.viewport.scrollAndZoomIntoView(nodes);
-  // }
+  if (msg.type === 'update') {
+    // Get the Lokalise project ID from the input
+
+    // Get the Lokalise API token from the input
+
+    // Finds all text layers with our naming convention in the current page
+    translatableLayersArray = figma.currentPage.findAll(node => {
+      return node.type === "TEXT" && node.name.includes("}}+{")
+    });
+  }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  // figma.closePlugin();
 };
+
+/////////////////////////
+// FUNCTIONS
+/////////////////////////
